ddcb66f1f367280ae05f68b9f0b6f69464967ef8
yuvraj371 <139117085+yuvraj371@users.noreply.github.com>
2023-07-15 17:19:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
 import os
 import logging
 class Config:                                                                   
-    API_ID = int(os.environ.get("API_ID", "12327086"))
-    API_HASH = os.environ.get("API_HASH", "b9f2b28ac8a9ee7a13d76f224d6a1b62")       
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6366543585:AAH3qX1_M0W3-SORnG8pVCbaETqna03pYcY")
-    BOT_SESSION = os.environ.get("BOT_SESSION", "forwardbot")
-    OWNER_ID = os.environ.get("OWNER_ID", "6310080453")                             
-    DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://krishna:krishna123@cluster1.ohpp83e.mongodb.net/?retryWrites=true&w=majority")  
-    DATABASE_NAME = os.environ.get("DATABASE_NAME", "krishna")
+    API_ID = int(os.environ.get("API_ID", ""))
+    API_HASH = os.environ.get("API_HASH", "")       
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_SESSION = os.environ.get("BOT_SESSION", "")
+    OWNER_ID = os.environ.get("OWNER_ID", "")                             
+    DATABASE_URI = os.environ.get("DATABASE_URI", "")  
+    DATABASE_NAME = os.environ.get("DATABASE_NAME", "")
     COLLECTION_NAME = os.environ.get('COLLECTION_NAME', 'Data')
-    SESSION = os.environ.get("SESSION", "BQC8GK4AMKOs3vq06nCudSCqjTUlOWV9qi7V8SWkIGf2CJJS9kYVGYPx71Xd5bzqVT87N_gXgvd9t_zQeRTncifXcC-mrB_cVLl3vfIA4rvSpKwx2bQq1yA0Kx9xFsw9VQ3TBy7nssMhjSTszdzMDMyyE6CxoavkGNtHkrCAxyCkcWik59YKv8xlrzlvFbwodqEitJ-9inJ77uJl5M2eqGEZ3_ccYuaTfGU2aBP7-9w3TTIbC3jhZ4LXNRfY8JHHBxafZm9v7YSrfCA4u4rLeBcOQfq7akpgKM6FjvGHsTJg3ztuRRzhUpluT9ZS3PPl3Ypn6sp9KU4LU1G2KU14JqT1e4ztIgAAAAF4HC_FAA")   
-    TO_CHANNEL = int(os.environ.get("TO_CHANNEL", "-1001690922221"))
-    BOT_USERNAME= os.environ.get("BOT_USERNAME", "Forward_infinity_bot")
+    SESSION = os.environ.get("SESSION", "")   
+    TO_CHANNEL = int(os.environ.get("TO_CHANNEL", ""))
+    BOT_USERNAME= os.environ.get("BOT_USERNAME", "")
 
 
 def LOGGER(name: str) -> logging.Logger:
